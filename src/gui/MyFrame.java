4ca1bc1c75f3ac8349cb50c51da6b709316810ff
Luís Melo <luismelo7@gmail.com>
2016-04-04 21:01:06 +0100

    minor fixes

--- src/gui/MyFrame.java
+++ src/gui/MyFrame.java
@@ -156,6 +156,13 @@ public class MyFrame extends JFrame{
 				//gamePanel.repaint(false);
 				build.setVisible(false);
 				gamePanel.setVisible(true);
+				
+				btnCreate.setText("Build New Maze");
+				/*build = new BuildPanel(false);				
+				build.setVisible(true);
+				build.setBounds(0, 25, 650, 650);
+				getContentPane().add(build);
+				build.setLayout(null);*/
 			}
 		});
 		home.setIcon(new ImageIcon("imgs/homeSymb.png"));
